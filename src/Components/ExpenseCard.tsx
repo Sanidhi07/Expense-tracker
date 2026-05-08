914430bbdfb4e89f6aabcd6ad90cd0c00b7fcb86
= <Sanidhi2207@gmail.com>
2026-05-08 16:11:47 +0530

    style: migrated ExpenseCard to tailwind classes

--- src/Components/ExpenseCard.tsx
+++ src/Components/ExpenseCard.tsx
@@ -5,13 +5,20 @@ interface ExpenseCardProps{
 }
 
 export const ExpenseCard=({expense}:ExpenseCardProps)=>{
-    return 
-    <div className=''>
-      <h3>{expense.title}</h3>
-      <p>Amount: ₹{expense.amount}</p>
-      <p>Category: <strong>{expense.category}</strong></p>
-      <small>ID: {expense.id}</small>
+    return (
+    <div className='bg-white shadow-md p-4 mb-4 rounded-xl flex justify-between items-center'>
+        <div>
+      <h3 className='text-lg font-bold text-slate-800 '>{expense.title}</h3>
+            <p className='text-sm text-slate-500'>Category: <strong>{expense.category}</strong></p>
+      
+      </div>
+      <div className='text-right'>
+        <p className='font-semibold text-green-600'>Amount: ₹{expense.amount}</p>
+       <small>ID: {expense.id}</small>
+      </div>
+      
         
     </div>
+    )
 
 }
